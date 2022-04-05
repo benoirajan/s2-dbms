196195148da1ed83c5ae85244423044172e5d0e2
benoirajan <benoirajan@gmail.com>
2022-04-05 11:51:09 +0530

    updates

--- 1.sql
+++ 1.sql
@@ -1,9 +1,11 @@
 a.    create table students(roll int, name varchar(30),course varchar(10),year int);
+      Table created.
 
 b.    alter table students add primary key(roll);
+      Table altered.
 
 c.    truncate table student;
-      Table truncated
+      Table truncated.
 
 d.    drop table student;
       Table dropped
