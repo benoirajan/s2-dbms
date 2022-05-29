143af2d5bc81de8cd4454735e7077f9d42ae2fd6
Benoi Rajan Abraham <95347562+benoirajan@users.noreply.github.com>
2022-05-29 18:40:12 +0530

    Add files via upload

---
+++ 11.sql
@@ -0,0 +1,11 @@
+17-5-22
+/*add two numbers*/ 
+declare
+    a integer := 5;
+    b integer := 3;
+    c integer;
+begin
+    c := a+b;
+    dbms_output.put_line(chr(10)||a||' + '||b||' = '||c);
+end;
+/
---
+++ 16.sql
@@ -0,0 +1,15 @@
+17-5-22
+-- procedure: square of a value
+declare
+    a number; 
+    b number; 
+procedure square(x in number, y out number) is 
+begin
+    y:= x*x;
+end;   
+begin 
+    a:= 5;  
+    square(a, b); 
+    dbms_output.put_line(chr(10)||'Square = ' || b); 
+end; 
+/
